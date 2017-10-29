3f7a2bb3f7ec02a2c9105a05451ad913054d1dad
xomajke <nikolay.blonskiy@gmail.com>
2017-10-29 15:10:02 +0200

    server and public folders

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const public_path = path.join(__dirname, '../public');
+
+app.use(express.static(public_path));
+
+app.listen(3000, () => console.log('Server started on 3000 port.'));
